c58d7218571698dd2c4f7f6d7daa5b151ba5c416
ANKIT KUMAR RAI <ankitkumarrai02612@gmail.com>
2025-12-14 23:02:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Consonants and Vowels check - Java/consonants-and-vowels-check-java.java
@@ -0,0 +1,28 @@
+class Geeks {
+
+    static void checkString(String s) {
+        int v = 0;
+        int c = 0;
+
+        // Your code here
+        String vowels = "aeiou";
+        for(char ch:s.toCharArray()){
+            if(vowels.indexOf(ch) != -1){
+                v++;
+            }else {
+                c++;
+            }
+        }
+
+        if (v > c)
+            System.out.print("Yes");
+        else if (c > v)
+            System.out.print("No");
+        else
+            System.out.print("Same");
+
+        System.out.println();
+    }
+}
+
+//Position this line where user code will be pasted.
