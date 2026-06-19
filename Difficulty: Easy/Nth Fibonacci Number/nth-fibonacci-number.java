d2726042251404ca6cf176db7d349251c44f3b17
ANKIT KUMAR RAI <ankitkumarrai02612@gmail.com>
2026-06-19 23:01:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.java
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.java
@@ -4,8 +4,8 @@ class Solution {
         int num1 = 0;
         int num2 = 1;
         int num3;
+        
         for(int i=0; i<n; i++){
-            // System.out.print(num1 + " ");
             num3 = num1 + num2;
             num1 = num2;
             num2 = num3;
